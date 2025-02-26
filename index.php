518457ac22e00073146a6f74d912151b676cf94c
Pedro <pedrottorres07@gmail.com>
2025-02-26 12:20:16 -0300

    Header Finalizado

--- index.php
+++ index.php
@@ -25,12 +25,12 @@
     
             <div class="icons">
             <!-- LINK DA IMAGEM DA LUPA -->
-            <svg width="30px" height="30px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="#000000"><path d="M15.25 0a8.25 8.25 0 0 0-6.18 13.72L1 
+            <svg width="30px" height="30px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="#ffffff"><path d="M15.25 0a8.25 8.25 0 0 0-6.18 13.72L1 
             22.88l1.12 1 8.05-9.12A8.251 8.251 0 1 0 15.25.01V0zm0 15a6.75 6.75 0 1 1 0-13.5 6.75 6.75 0 0 1 0 13.5z"/></svg>
 
             <!-- LINK DA IMAGEM DO CARRINHO -->
             <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M2 2h4v4h16v11H4V4H2V2zm4 13h14V8H6v7zm0 4h3v3H6v-3zm14 0h-3v3h3v-3z" fill="#000000"/></svg>
+            <path d="M2 2h4v4h16v11H4V4H2V2zm4 13h14V8H6v7zm0 4h3v3H6v-3zm14 0h-3v3h3v-3z" fill="#ffffff"/></svg>
 
             </div>
     </section>

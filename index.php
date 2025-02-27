9d86512238ac81ef0a0dfeb147aad63a32609c58
Pedro <pedrottorres07@gmail.com>
2025-02-27 10:32:36 -0300

    produtos na index

--- index.php
+++ index.php
@@ -1,48 +1,53 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Lilita+One&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="Css/styles.css">
-    
+
     <title>Cafeteria</title>
 </head>
-<body>   
-<!-- Inicio Header -->
+
+<body>
+    <!-- Inicio Header -->
 
     <header class="header">
         <section>
             <a href="" class="logo">
                 <img src="Img/menu-1.png" alt="">
             </a>
-            
-        <nav class="navbar">
-           <a href="">Home</a>
-           <a href="#sobre">Sobre</a>
-           <a href="#menu">Menu</a>
-           <a href="#avaliacao">Avaliações</a>
-           <a href="#endereco">Endereço</a>  
-        </nav>
-    
-            <div class="icons">
-            <!-- LINK DA IMAGEM DA LUPA -->
-            <svg width="30px" height="30px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="#ffffff"><path d="M15.25 0a8.25 8.25 0 0 0-6.18 13.72L1 
-            22.88l1.12 1 8.05-9.12A8.251 8.251 0 1 0 15.25.01V0zm0 15a6.75 6.75 0 1 1 0-13.5 6.75 6.75 0 0 1 0 13.5z"/></svg>
 
-            <!-- LINK DA IMAGEM DO CARRINHO -->
-            <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M2 2h4v4h16v11H4V4H2V2zm4 13h14V8H6v7zm0 4h3v3H6v-3zm14 0h-3v3h3v-3z" fill="#ffffff"/></svg>
+            <nav class="navbar">
+                <a href="">Home</a>
+                <a href="#sobre">Sobre</a>
+                <a href="#menu">Menu</a>
+                <a href="#avaliacao">Avaliações</a>
+                <a href="#endereco">Endereço</a>
+            </nav>
+
+            <div class="icons">
+                <!-- LINK DA IMAGEM DA LUPA -->
+                <svg width="30px" height="30px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="#ffffff">
+                    <path d="M15.25 0a8.25 8.25 0 0 0-6.18 13.72L1 
+            22.88l1.12 1 8.05-9.12A8.251 8.251 0 1 0 15.25.01V0zm0 15a6.75 6.75 0 1 1 0-13.5 6.75 6.75 0 0 1 0 13.5z" />
+                </svg>
+
+                <!-- LINK DA IMAGEM DO CARRINHO -->
+                <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M2 2h4v4h16v11H4V4H2V2zm4 13h14V8H6v7zm0 4h3v3H6v-3zm14 0h-3v3h3v-3z" fill="#ffffff" />
+                </svg>
 
             </div>
-    </section>
- </header>
+        </section>
+    </header>
     <!-- FIM HEADER  -->
 
     <!-- Começo Banner  -->
-     <img src="Img/Design sem nome (3).png" alt="" class="banner">
+    <img src="Img/Design sem nome (3).png" alt="" class="banner">
     <!-- Fim Banner  -->
-    
+
     <!-- Começo sobre nós  -->
     <section class="about" id="about">
         <h2 class="title">Sobre <span>Nós</span> </h2>
@@ -60,15 +65,61 @@
                 </p>
                 <br>
                 <br>
-                    <a href="" class="btn">Saiba Mais </a>
+                <a href="#sobre" class="btn botao">Saiba Mais </a>
             </div>
-
         </div>
     </section>
+    <!-- Fim sobre nós  -->
+    <h2 class="title">Produ <span>tos</span> </h2>
+ 
+        <div class="flex">
+            <div class="card">
+                <img src="Img/cafe2.jpg" alt="Denim Jeans" style="width:100%">
+                <h1>Café Expresso</h1><br>
+                <p class="price">R$19.99</p>
+                <br>
+                <br>
+                <p>Some text about the jeans. Super slim and comfyb <br> lorem ipsum lorem jeansum. Lorem jeamsun denim lorem jeansum.</p><br>
+                <br>
+                <p><button>Add to Cart</button></p>
+            </div>
 
-
-    <!-- Fim sobre nos  -->
+            <div class="card">
+                <img src="Img/cafe2.jpg" alt="Denim Jeans" style="width:100%">
+                <h1>Café Expresso</h1><br>
+                <p class="price">R$19.99</p>
+                <br>
+                <br>
+                <p>Some text about the jeans. Super slim and comfy <br>lorem ipsum lorem jeansum. Lorem jeamsun denim lorem jeansum.</p><br>
+                <br>
+                <p><button>Add to Cart</button></p>
+            </div>
+           
+                <div class="card">
+                    <img src="Img/cafe2.jpg" alt="Denim Jeans" style="width:100%">
+                    <h1>Café Expresso</h1><br>
+                    <p class="price">R$19.99</p>
+                    <br>
+                    <br>
+                    <p>Some text about the jeans. Super slim and comfy <br> lorem ipsum lorem jeansum. Lorem jeamsun denim lorem jeansum.</p><br>
+                    <br>
+                    <p><button>Add to Cart</button></p>
+                </div>
+            
+            
+                <div class="card">
+                    <img src="Img/cafe2.jpg" alt="Denim Jeans" style="width:100%">
+                    <h1>Café Expresso</h1><br>
+                    <p class="price">R$19.99</p><br><br>
+                    <p>Some text about the jeans. Super slim and comfy <br> lorem ipsum lorem jeansum. Lorem jeamsun denim lorem jeansum.</p><br>
+                    <br>
+                    <p><button>Add to Cart</button></p>
+    
+            </div>
+    </div>
+        <!-- Fim sobre nos  -->
 
 
 </body>
+
 </html>
